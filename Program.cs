38d86f2163d230f0faf1b6eeae6737b3a9a840d1
Jonathan-Wills <wills830@hotmail.com>
2022-09-01 00:40:53 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperatorsExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double areaofcircle = AreaOfCircle(radius);
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+        
+}
